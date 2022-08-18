1cde18008c3d346aa3e51e6e9afe2ec9ac502bd2
David <developerdavid00@gmail.com>
2022-08-18 17:22:11 -0300

    Projeto Final

---
+++ src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    heigth: 150px;
+    text-align: center;
+    background: #00C6E0;    
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+    padding: 70px;
+    color: black;
+    font-size: 40px;
+    font-weight: bolder;
+`;
---
+++ src/styles/Global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+const Global = createGlobalStyle `
+
+*{
+    margin: 0;
+    paddin: 0;
+}
+
+body {
+    font-family: 'Ibarra Real Nova', serif;
+    background-color: #f2f2f2;
+ }
+`
+
+export default Global;
